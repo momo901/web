dd8b6f5d5b0f4644934840ad9ff31c642b7a73ba
Boubouh Karim <mister_karim@hotmail.fr>
2017-11-23 02:30:43 +0000

    integrate ANT design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { NZ_LOCALE, enUS } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_LOCALE, useValue: enUS }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
