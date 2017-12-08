521ac0819cc2762d30b3359384139200011a53a1
Boubouh Karim <mister_karim@hotmail.fr>
2017-12-08 04:03:16 +0000

    some tweaks to make header responsive & updating packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { NZ_LOCALE, enUS } from 'ng-zorro-antd';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
-    NgZorroAntdModule.forRoot()
+    NgZorroAntdModule.forRoot(),
+    NgbModule.forRoot()
   ],
   providers: [
     { provide: NZ_LOCALE, useValue: enUS }
